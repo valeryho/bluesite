b872ae36da9a203077ad1b3f237e9b972511ca9b
valeryho <v.gorodskov1985@gmail.com>
2019-02-18 16:07:27 +0200

    Aded why us

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    //var instances = M.Dropdown.init(elems, options);
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
